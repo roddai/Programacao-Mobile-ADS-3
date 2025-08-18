31493928aa2e4bf8c2b65aa58a8d4eaccfe105cc
27532544869 <roddai@hotmail.com>
2025-08-18 17:42:35 -0300

    commit inicial da aula 03

--- Aula01/app/build.gradle.kts
+++ Aula01/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.aula01_project"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.aula01_project"
