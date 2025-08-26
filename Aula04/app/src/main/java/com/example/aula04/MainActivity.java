01fb336202eeeb2fa2940545e4ceb04b4fcf2d40
24026770 <guroberto.dev@gmail.com>
2025-08-25 22:27:26 -0300

    Segunda  atividade com interação entre os botões

---
+++ Aula04/app/src/main/java/com/example/aula04/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.aula04;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editNome;
+    private Button btnEnviar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // Layout da primeira tela
+
+        editNome = findViewById(R.id.editNome);
+        btnEnviar = findViewById(R.id.btnEnviar);
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nomeDigitado = editNome.getText().toString();
+
+                Intent irParaSegundaTela = new Intent(MainActivity.this, SegundaActivity.class);
+                irParaSegundaTela.putExtra("nomeUsuario", nomeDigitado);
+                startActivity(irParaSegundaTela);
+            }
+        });
+    }
+}
---
+++ Aula04/app/src/main/java/com/example/aula04/SegundaActivity.java
@@ -0,0 +1,24 @@
+package com.example.aula04;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SegundaActivity extends AppCompatActivity {
+
+    private TextView txtSaudacao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda); // Layout da segunda tela
+
+        txtSaudacao = findViewById(R.id.txtSaudacao);
+
+        // Pegando o nome enviado da MainActivity
+        String nomeRecebido = getIntent().getStringExtra("nomeUsuario");
+
+        txtSaudacao.setText("Olá, " + nomeRecebido + "!");
+    }
+}
