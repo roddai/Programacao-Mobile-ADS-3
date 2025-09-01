dc631d03c85687d935d6af54221492b46e889e7f
24026623 <renandamprelli@gmail.com>
2025-09-01 19:49:18 -0300

    refactor: código novo "Aula 05" IMC

--- app/src/main/java/com/example/java_aula_mobile/MainActivity.java
+++ app/src/main/java/com/example/java_aula_mobile/MainActivity.java
@@ -5,30 +5,42 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText editNome;
-    private Button btnEnviar;
+    EditText editPeso, editAltura;
+    Button btnCalcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // Layout da primeira tela
+        setContentView(R.layout.activity_main);
 
-        editNome = findViewById(R.id.editNome);
-        btnEnviar = findViewById(R.id.btnEnviar);
+        editPeso = findViewById(R.id.editPeso);
+        editAltura = findViewById(R.id.editAltura);
+        btnCalcular = findViewById(R.id.btnCalcular);
 
-        btnEnviar.setOnClickListener(new View.OnClickListener() {
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String nomeDigitado = editNome.getText().toString();
+                double peso = Double.parseDouble(editPeso.getText().toString());
+                double altura = Double.parseDouble(editAltura.getText().toString());
+                double imc = peso / (altura * altura);
 
-                Intent irParaSegundaTela = new Intent(MainActivity.this, SegundaActivity.class);
-                irParaSegundaTela.putExtra("nomeUsuario", nomeDigitado);
-                startActivity(irParaSegundaTela);
+                Intent intent;
+                if (imc < 18.5) {
+                    intent = new Intent(MainActivity.this, ResultadoAbaixoDoPesoActivity.class);
+                } else if (imc < 25) {
+                    intent = new Intent(MainActivity.this, ResultadoNormalActivity.class);
+                } else if (imc < 30) {
+                    intent = new Intent(MainActivity.this, ResultadoSobrepesoActivity.class);
+                } else if (imc < 35) {
+                    intent = new Intent(MainActivity.this, ResultadoObesidadeClasse1Activity.class);
+                } else {
+                    intent = new Intent(MainActivity.this, ResultadoObesidadeClasse2Activity.class);
+                }
+                startActivity(intent);
             }
         });
     }
