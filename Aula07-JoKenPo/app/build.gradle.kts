b1961d5c278afe9656dd898bb2a8c2ca2f88462b
27532544869 <roddai@hotmail.com>
2025-09-29 16:14:21 -0300

    commit da aula 07

---
+++ Aula07-JoKenPo/app/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.jokenpo"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.jokenpo"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildToolsVersion = "36.0.0"
+    compileOptions {
+        targetCompatibility = VERSION_1_8
+        sourceCompatibility = VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
