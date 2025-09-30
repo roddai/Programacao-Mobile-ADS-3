94177193da8c8b20e7232dec1884e61474b5ba89
27532544869 <roddai@hotmail.com>
2025-09-29 22:39:28 -0300

    adiciona projeto Aula 08

---
+++ Aula08/app/src/main/java/com/example/projetoaula08/ResultadoActivity.java
@@ -0,0 +1,24 @@
+package com.example.projetoaula08;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultadoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado);
+
+        TextView tvResultado = findViewById(R.id.tvResultado);
+        Button btnVoltar = findViewById(R.id.btnVoltar);
+
+        String resultado = getIntent().getStringExtra("resultado");
+        tvResultado.setText(resultado);
+
+        btnVoltar.setOnClickListener(v -> finish());
+    }
+}
