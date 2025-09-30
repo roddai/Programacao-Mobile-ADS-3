960b3c1d2d5de30bf815d4cfdae791122218d223
24026851 <vivini13235@gmail.com>
2025-09-29 22:28:28 -0300

    Implementação de checkbox, radio e spinner.

---
+++ Aula8/app/src/main/java/com/example/aula8/ResultadoActivity.java
@@ -0,0 +1,29 @@
+package com.example.aula8;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ResultadoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado);
+
+        TextView tvResultado = findViewById(R.id.tvResultado);
+        Button btnVoltar = findViewById(R.id.btnVoltar);
+
+        String resultado = getIntent().getStringExtra("resultado");
+        tvResultado.setText(resultado);
+
+        btnVoltar.setOnClickListener(v -> finish());
+    }
+}
