e2b6376d3ea02691575333f193e2d58e255ffc70
Luan Rocha da Silva <luanroccha13@gmail.com>
2025-10-06 22:21:02 -0300

    Aula09Tarefa

---
+++ Aula05/app/src/main/java/com/example/project05/ResultadoObesidadeClasse1Activity.java
@@ -0,0 +1,12 @@
+package com.example.project05;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultadoObesidadeClasse1Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado_obesidade_1);
+    }
+}
